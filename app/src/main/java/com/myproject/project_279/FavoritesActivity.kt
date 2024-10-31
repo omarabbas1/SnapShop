ae8007b05b95f84690be850f3432801facf1bce1
layana kreidly <layana1.kreidly@gmail.com>
2024-10-31 17:43:29 +0200

    added favorites feature

---
+++ app/src/main/java/com/myproject/project_279/FavoritesActivity.kt
@@ -0,0 +1,11 @@
+package com.myproject.project_279
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FavoritesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorites)
+    }
+}
